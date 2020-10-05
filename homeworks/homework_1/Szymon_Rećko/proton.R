6b17a0f4ea01f9a022c6bab27dc4cc9f56de9894
Szymon164 <56126580+Szymon164@users.noreply.github.com>
2020-10-05 16:50:09 +0200

    1

---
+++ homeworks/homework_1/Szymon_Rećko/proton.R
@@ -0,0 +1,31 @@
+library(stringr)
+library(dplyr)
+library(PogromcyDanych)
+library(proton)
+proton()
+
+#1
+employees %>%
+  filter(name=="John",surname=="Insecure")
+proton(action = "login", login="johnins")
+
+#2
+for(super_trudne_haslo_ktorego_nikt_nie_zgadnie in top1000passwords)
+{
+  proton(action = "login", login="johnins", password=super_trudne_haslo_ktorego_nikt_nie_zgadnie)
+}
+
+#3
+employees %>% 
+  filter(surname == "Pietraszko")
+logs %>% 
+  filter(login == "slap") %>% 
+  count(host)
+proton(action = "server", host="194.29.178.16")
+
+#4
+bash_history[!str_detect(bash_history, " ")] %>% 
+  data.frame() %>% 
+  distinct()
+proton(action = "login", login="slap", password = "DHbb7QXppuHnaXGN")
+
