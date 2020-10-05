8c578c8eab2f1b37fb8210b5ee909292af37da71
adrian22311 <56119717+adrian22311@users.noreply.github.com>
2020-10-05 18:21:46 +0200

    Proton

---
+++ homeworks/homework_1/kaminski_adrian/kaminski_adrian.R
@@ -0,0 +1,48 @@
+# Zadanie 1
+# Wykonać zadanie z biblioteki proton
+
+library(proton)
+library(dplyr)
+
+proton()
+
+employees %>%
+  filter(name == "John", surname == "Insecure") %>%
+  select(login) -> john_login
+
+proton(action = "login", login=john_login)
+
+
+sapply(
+  top1000passwords,
+       function(x) proton(action = "login",
+                          login = john_login,
+                          password=x)
+       ) # lepsze od petli
+
+employees %>%
+  filter(surname == "Pietraszko") %>%
+  select(login) %>%
+  as.character() -> login_celu
+
+logs %>%
+  filter(login == login_celu) %>%
+  count(host, name = "liczba") %>% 
+  arrange(-liczba) %>%
+  head(1) %>%
+  select(host) -> host_celu
+
+proton(action = "server", host="194.29.178.16") # wpisane recznie bo (typeof) 
+                                                # host_celu wyszedl factor i nie moge sobie z nim poradzic
+
+library(stringi)
+
+unique(
+  bash_history[!stri_detect_fixed(bash_history, " ")]
+  ) -> mozliwe_hasla
+
+stri_sort(mozliwe_hasla) -> hasla_posortowane # nie konieczne ale moglo byc wiecej i wtedy moze ulatwic znalezienie
+haslo_celu <- hasla_posortowane[1]
+
+proton(action = "login", login=login_celu, password=haslo_celu)
+
