207d258e58fd5b47326a08c5c74a701f082c07b9
jusira <alicjagosiewska@gmail.com>
2020-10-12 12:21:49 +0200

    Update week 2

--- labs/group_2/lab_2.R
+++ labs/group_2/lab_2.R
@@ -13,6 +13,7 @@ head(serialeIMDB)
 
 # color
 
+
 # size
 
 
@@ -21,10 +22,21 @@ head(serialeIMDB)
 
 # geom_bar()
 
-# color vs fill
+
+# geom_boxplot()
+
+
+# geom_violin()
 
 
 # geom_histogram()
+
+
+data("maturaExam")
+head(maturaExam)
+# jak zwizualizowac?
+
+
 
 
 
@@ -36,13 +48,23 @@ data("maturaExam")
 head(maturaExam)
 
 
-
 # Zadanie 2
-#Narysować wykres średniej liczby punktów w zaleności od roku i przemdiotu.
-
+# Narysować wykres średniej liczby punktów w zaleności od roku i przedmiotu.
 
 data("maturaExam")
 head(maturaExam)
 
 
+# Zadanie 3
+# Narysować zależność prebiegu od roku produkcji. Ilu różnych wykresów da się użyć do tego celu?
+
+data("auta2012")
+head(auta2012)
+
+
+# Zadanie 4
+# Narysować zależność ceny od Marki. Ile róznych wykresów da się narysować?
+
+data("auta2012")
+head(auta2012)
 
